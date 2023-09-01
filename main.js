09b0e5766cc0997182180eb1e5a245718ee8d59d
Higor Santos <higor.stos@outlook.com>
2023-09-01 00:54:37 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(numA, numB) {
+    return console.log(numA * numB);
+}
+function saudacao(nome) {
+    return console.log('Olá ' + nome);
+}
+multiplicacao(5, 10);
+saudacao('Higor');
---
+++ main.ts
@@ -0,0 +1,10 @@
+function multiplicacao(numA: number, numB: number): number | void {
+    return console.log(numA * numB);
+}
+
+function saudacao(nome: string): string | void {
+    return console.log('Olá ' + nome);
+}
+
+multiplicacao(5, 10);
+saudacao('Higor');
