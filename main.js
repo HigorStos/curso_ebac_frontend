544ac41bfd859cb591be77734d3f53cb76057bca
Higor Santos <higor.stos@outlook.com>
2023-09-05 01:15:26 -0300

    fix

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
 "use strict";
-const multiplicacao = (numA, numB) => console.log(numA * numB);
-const saudacao = (nome) => console.log('Olá ' + nome);
-multiplicacao(5, 10);
-saudacao('Higor');
+const multiplicacao = (numA, numB) => numA * numB;
+const saudacao = (nome) => 'Olá ' + nome;
